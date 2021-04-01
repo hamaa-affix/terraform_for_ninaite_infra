e3bba1aad1d5a9caa6d7be0b61d2a754be5490ac
hamaa-affix <hamaagive1108@gmail.com>
2021-04-01 23:23:52 +0900

    create subnet and routetable

---
+++ dev/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "project" {
+  default = "ninaite"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "availability_zones" {
+  type = list(string)
+  default = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+}
